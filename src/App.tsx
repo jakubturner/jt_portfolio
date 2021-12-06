70ababd750083da1718c44221ef9dbccac5204c8
Jakub Turner <jt@Jakubs-MacBook-Pro.local>
2021-12-06 09:45:10 +0100

    create mobile version h1...

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ function App() {
         <main className='bgGradient w-screen h-screen relative'>
             <div className='bg-main-pattern w-full h-full absolute top-0 left-0 bg-cover bg-center'></div>
             <div>
-                <h1 className='text-mintGreen-default'>Mrdat</h1>
+              <div className='pl-8 pr-8 pt-16' >
+                <h1 className='text-mintGreen-default w-32 h-8 font-black text-4xl inline'>Hi, I'm JT</h1>
+                <p className='text-mintGreen-default font-medium text-sm mt-4'>React Developer. Currently working full-time as a freelance React Developer for Cogvio. Learning R.Native</p>
+                </div>
             </div>
         </main>
   );
